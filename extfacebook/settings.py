5fc206322f5a2eb3f6d33350cfe54767fdf79644
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2013-06-05 15:25:44 +0200

    commit

--- extfacebook/settings.py
+++ extfacebook/settings.py
@@ -1,11 +1,15 @@
 from django.conf import settings
 
-FACEBOOK_AUTH_PERMISSION = getattr(settings,'FACEBOOK_AUTH_PERMISSION',['email',])
+permission = ['email','user_likes','user_hometown','user_location','user_birthday','user_online_presence',
+              'publish_actions','read_stream']
+
+FACEBOOK_AUTH_PERMISSION = getattr(settings,'FACEBOOK_AUTH_PERMISSION',permission)
 
 if not isinstance(FACEBOOK_AUTH_PERMISSION,list):
     raise BaseException("FACEBOOK_AUTH_PERMISSION is not a list")
 
 try:
-    FACEBOOK_AUTH_PERMISSION.index("email")
+    for perm in permission:
+        FACEBOOK_AUTH_PERMISSION.index(perm)
 except ValueError:
-    raise BaseException("email permission in FACEBOOK_AUTH_PERMISSION is missing")
+    raise BaseException(" %s permissions in FACEBOOK_AUTH_PERMISSION are missing" % (" ".join(permission)))
